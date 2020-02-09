b8376364c0961abb782cdd1e951d67d403b3d0d1
XodTuza <gordie2u@gmail.com>
2020-02-09 02:14:52 +0100

    first commit

---
+++ www/js/script.js
@@ -0,0 +1,35 @@
+function adSetter(){
+alert(navigator.userAgent);
+ 
+var admobid = {};
+if( /(android)/i.test(navigator.userAgent) ) { // for android & amazon-fireos
+  admobid = {
+    banner: 'ca-app-pub-3940256099942544/6300978111', // or DFP format "/6253334/dfp_example_ad"
+    interstitial: 'ca-app-pub-3940256099942544/1033173712'
+  };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+  admobid = {
+    banner: 'ca-app-pub-3940256099942544/6300978111', // or DFP format "/6253334/dfp_example_ad"
+    interstitial: 'ca-app-pub-3940256099942544/1033173712'
+  };
+} else { // for windows phone
+  admobid = {
+    banner: 'ca-app-pub-3940256099942544/6300978111', // or DFP format "/6253334/dfp_example_ad"
+    interstitial: 'ca-app-pub-3940256099942544/1033173712'
+  };
+}
+
+if(AdMob) AdMob.createBanner({
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.TOP_CENTER,
+    autoShow: true });
+}
+
+function onDeviceReady() {
+    alert("device ready")
+    adSetter();
+}
+
+function donLoaded() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
